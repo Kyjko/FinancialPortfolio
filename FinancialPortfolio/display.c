38596961d74f0ff64e64a62799e5d416a5518121
Kyjko <bognarmiklos34@gmail.com>
2021-04-04 20:23:04 +0200

    minor update

--- FinancialPortfolio/display.c
+++ FinancialPortfolio/display.c
@@ -16,6 +16,21 @@ __declspec(noalias) void _Display(const float* __restrict data, const size_t len
 		return;
 	}
 
+	float* normalized_data = (float*)malloc(sizeof(float) * len);
+	if (normalized_data == NULL) {
+		return;
+	}
+	float max = 0;
+	for (size_t i = 0; i < len; i++) {
+		if (data[i] >= max) {
+			max = data[i];
+		}
+	}
+	
+	for (size_t i = 0; i < len; i++) {
+		normalized_data[i] = data[i] / max;
+	}
+
 	short quit = 0;
 	while (quit != 1) {
 		SDL_Event e;
@@ -31,7 +46,7 @@ __declspec(noalias) void _Display(const float* __restrict data, const size_t len
 		
 		SDL_SetRenderDrawColor(r, 180, 255, 0, 255);
 		for (size_t i = 0; i < len - 1; i++) {
-			SDL_RenderDrawLine(r, i * W / len, H / 2 - (data[i])*2, (i + 1) * W / len, H / 2 - (data[i+1])*2);
+			SDL_RenderDrawLine(r, i * W / len, H / 2 - (normalized_data[i])*100, (i + 1) * W / len, H / 2 - (normalized_data[i+1])*100);
 		}
 
 		SDL_SetRenderDrawColor(r, 255, 180, 0, 255);
@@ -41,6 +56,7 @@ __declspec(noalias) void _Display(const float* __restrict data, const size_t len
 		SDL_RenderPresent(r);
 	}
 
+	free(normalized_data);
 	SDL_DestroyWindow(w);
 	SDL_DestroyRenderer(r);
 	SDL_Quit();
